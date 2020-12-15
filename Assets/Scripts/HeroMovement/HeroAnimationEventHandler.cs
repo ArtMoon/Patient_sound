12d5a1a65d68140b0edd40b7cc22ebd7b378b804
DESKTOP-FG1JAAJ\Vader <Vader>
2020-12-15 22:41:39 +0300

    Test sounds Added

---
+++ Assets/Scripts/HeroMovement/HeroAnimationEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAnimationEventHandler : MonoBehaviour
+{
+
+    private AudioSource _source;
+
+    private void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+    public void HeroStepSound()
+    {
+        _source.Stop();
+        _source.Play();
+    }
+}
